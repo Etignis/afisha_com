698735d866561a8777eb2e53279b9919db487c58
Tenebrius <etignis@gmail.com>
2019-03-28 17:40:17 +0300

    wipwip

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,116 @@
-function randd(min, max) {
-  return Math.floor(arguments.length > 1 ? (max - min + 1) * Math.random() + min : (min + 1) * Math.random());
-};
+Vue.component('modalWin', {
+	props: {
+		title: {
+			type: String,
+			default: ""
+		},
+		content: {
+			type: String,
+			default: ""
+		}
+	},
+	data: function(){
+		return {
+			
+		};
+	},
+	methods: {
+		close: function(){
+			this.$emit('close');
+		}
+	},
+	computed: {
+		
+	},
+
+	template: `<div class="mod_win_wrapper" style='background: rgba(0, 0, 0, 0.7);'  @scroll.stop>
+	<div class="mod_win">
+		<span class="bCloseInfoWin" @click="close">×</span>
+		<slot></slot>
+	</div>
+</div>`
+});
+
+Vue.component('combobox', {
+	props: {
+		id: {
+			type: String,
+			default: ""
+		}, 
+		val: {
+			type: String,
+			default: ""
+		},
+		list: {
+			type: Array
+		}
+	},
+	data: function(){
+		return {
+			listVisible: false,
+			innerId: "",
+			filteredList: []
+		};
+	},
+	methods: {
+		input: function(){
+			this.filterList();
+			this.$emit("input", this.value);
+		},
+		select: function(sText){
+			//this.value = sText;
+			this.listVisible = false;
+			this.$emit("input", sText);
+		},
+		filterList: function() {
+			let oRet = [];
+			let oInput =  this.$refs[this.innerId];
+			
+			if(oInput && oInput === document.activeElement) {
+				oRet = this.value.length>1? this.list.filter(el => el.trim().toUpperCase().indexOf(this.value.trim().toUpperCase())>-1) :this.list;
+			}
+			this.listVisible = oRet.length > 0;
+			this.filteredList = oRet;
+		},
+		toggleList: function(){
+			this.listVisible = !this.listVisible;
+		}
+	},
+	computed: {
+		value: function(){
+			return this.val;
+		}
+	},
+	created: function(){
+		//this.value = this.val;
+		this.innerId = this.Id || "combobox_input_"+Date.now();
+		this.filteredList = this.list;
+	},
+
+	template: `<div class="combobox">
+	<div class='input_place'>
+		<input 
+			type="text" 
+			:ref="innerId"
+			v-model="value" 
+			@input="input">
+		<button
+			@click="toggleList"
+		>▼</button>
+	</div>
+	<div class="combobox_list_place">
+		<ul class="combobox_list" v-show="listVisible">
+			<li
+				v-for="item in filteredList"
+				@click="select(item)"
+			>
+			{{item}}
+			</li>
+		</ul>
+	</div>
+</div>`
+});
+
 
 
 Vue.component('af_month', {
@@ -16,14 +126,10 @@ Vue.component('af_month', {
 		};
 	},
 	methods: {
-		labelClick: function(oEvent){
-			this.$emit('lclick', this.val);
-		}
+		
 	},
 	computed: {
-		id: function(){
-			return "ch_"+this.val;
-		}
+		
 	},
 	created: function(){
 		
@@ -38,6 +144,264 @@ Vue.component('af_month', {
 	</div>
 	<div class='af_mont_content'>
 		<slot></slot>
+	</div>
+</div>`
+});
+
+Vue.component('af_editor', {
+	props: {		
+		mode: {
+			type: String,
+			default: "add"
+		},
+		id: {
+			type: String,
+			default: ""
+		},
+		name: {
+			type: String,
+			default: ""
+		},
+		info: {
+			type: String,
+			default: ""
+		},		
+		place: {
+			type: String,
+			default: ""
+		},
+		age_limit: {
+			type: String,
+			default: ""
+		},
+		date: {
+			type: String,
+			default: ""
+		},
+		time: {
+			type: String,
+			default: ""
+		},
+		plays: {
+			type: Array
+		},
+		places: {
+			type: Array
+		},
+		ages: {
+			type: Array
+		}
+	},
+	data: function(){
+		return {
+			selectedPlayId: "",
+			//infoText: "",
+			localDate: "",
+			localTime: "",
+			localPlace: "",
+			localAge: "",
+			localInfo: ""
+		};
+	},
+	methods: {
+		init: function() {
+			/*/
+			let oPlay = this.plays.find(el => el.name == this.name);
+			this.selectedPlayId = oPlay? oPlay.id : "";
+			this.infoText = this.info.replace(/\|\|/g, "\n");
+			this.innerDate = this.date;
+			this.innerTime = this.time || "18:00:00";
+			this.innerPlace = this.place;
+			this.innerAge = this.age_limit;
+			/**/
+		},
+		
+		placeChanged: function(sText) {
+			this.innerPlace = sText;
+		},
+		ageChanged: function(sText) {
+			this.innerAge = sText;
+		},
+		
+		cancel: function() {
+			this.$emit('cancel');
+		},
+		submit: function() {
+			var oData = {
+				afisha_id: this.id,
+				play_id: this.selectedPlayId,
+				date: this.innerDate,
+				time: this.innerTime,
+				info: this.innerlInfo,
+				place: this.innerPlace,
+				age: this.innerAge
+			};
+			this.$emit('submit', oData);
+		},
+		
+		infoEdited: function(sVal) {
+			this.localInfo = sVal.data;
+		}
+	},
+	computed: {
+		
+		ageList: function(){
+			return this.ages.map(el => el.name);
+		},
+		placeList: function(){
+			return this.places.map(el => el.name);
+		},
+		infoRows: function(){
+			return this.infoText? this.infoText.split(/[\r\n]+/).length+1 : 2;
+		},
+		
+		editMode: function() {
+			return this.mode == "edit";
+		},
+		addMode: function() {
+			return this.mode == "add";
+		},
+		
+		innerInfo: {
+			get: function() {
+				return this.localInfo || this.info.replace(/\|\|/g, "\n");
+			},
+			set: function(sVal) {
+				this.localInfo = sVal;
+			}
+		},
+		innerDate: {
+			get: function() {
+				return this.localDate || this.date;
+			},
+			set: function(sVal) {
+				this.localDate = sVal;
+			}
+		},
+		innerTime: {
+			get: function() {
+				return this.localTime || this.time;
+			},
+			set: function(sVal) {
+				this.localTime = sVal;
+			}
+		},
+		innerPlace: {
+			get: function() {
+				return this.localPlace || this.place;
+			},
+			set: function(sVal) {
+				this.localPlace = sVal;
+			}
+		},
+		innerAge: {
+			get: function() {
+				return this.localAge || this.age_limit;
+			},
+			set: function(sVal) {
+				this.localAge = sVal;
+			}
+		},
+		innerPlayId: {
+			get: function() {
+				let oPlay = this.plays.find(el => el.name == this.name);
+				return this.selectedPlayId || (oPlay? oPlay.id : "");
+			},
+			set: function(sVal) {
+				this.selectedPlayId = sVal;
+			}
+		},
+		
+	},
+	created: function(){
+		
+	},
+	beforeUpdate: function(){
+		
+	},
+	beforeMount: function() {
+		
+	},
+	template: `<div class='af_editor'>
+	<table class='af_editor_table'>
+		<tr>
+			<td width="1">
+				Спектакль
+			</td>
+			<td>
+				<select v-model="innerPlayId">
+					<option
+						v-for="item in plays"
+						:value="item.id"
+					>
+					{{item.name}}
+					</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Дата
+			</td>
+			<td>
+				<input type="date" v-model="innerDate">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Время
+			</td>
+			<td>
+				<input type="time" v-model="innerTime">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Информация
+			</td>
+			<td>
+				<textarea class='af_row_body_info'  v-model="innerInfo" :rows="infoRows" style="width: 100%">
+				
+				</textarea>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Место проведения
+			</td>
+			<td>
+				<combobox
+					:val="innerPlace"
+					:list="placeList"
+					@input="placeChanged($event)"
+				>
+				</combobox>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Возрастное ограничение
+			</td>
+			<td>
+				<combobox
+					:val="innerAge"
+					:list="ageList"
+					@input="ageChanged($event)"
+				>
+				</combobox>
+			</td>
+		</tr>
+	</table>
+	<div class='af_editor_footer'>
+		<button class="btn_cancel" @click="cancel">
+			Отменить
+		</button>
+		<button class="btn_submit" @click="submit" v-show="editMode">
+			Сохранить
+		</button>
+		<button class="btn_submit" @click="submit" v-show="addMode">
+			Добавить
+		</button>
 	</div>
 </div>`
 });
@@ -100,7 +464,8 @@ Vue.component('af_item', {
 	},
 	methods: {
 		edit: function(){
-			this.editMode = !this.editMode;
+			//this.editMode = !this.editMode;
+			this.$emit('edit', this.id);
 		},
 		cancel: function(){
 			this.editMode = !this.editMode;
@@ -127,7 +492,7 @@ Vue.component('af_item', {
 		
 	},
 	template: `<div class='af_row'>
-	<div class='af_row_viewer' v-if="shown">
+	<div class='af_row_viewer' v-show="shown">
 		<div class='af_row_content'>		
 			<div class='af_row_datetime'>
 				<div class='af_row_date'>
@@ -142,10 +507,10 @@ Vue.component('af_item', {
 			</div>
 			
 			<div class='af_row_body'>
-				<div class='af_row_body_name'>
+				<div class='af_row_body_min'>
 					<a :href='url'>{{name}}</a>
 				</div>
-				<div class='af_row_body_img'>
+				<div class='af_row_body_max'>
 					<a :href='url'> 
 						<img :src="mainImage" :alt="name"/>
 					</a>
@@ -176,6 +541,14 @@ Vue.component('af_item', {
 		</div>
 	</div>
 	<div class='af_row_editor' v-if="editMode">
+		<af_editor
+			:id="id"
+			:name="name"
+			:info="info"
+			:place="place"
+			:age_limit="age_limit"
+		>
+		</af_editor>
 		<div class='af_row_panel'>
 			<a href="" @click.stop.prevent="cancel">Отменить</a>
 			<a href="" @click.stop.prevent="save">Сохранить</a>
@@ -190,6 +563,7 @@ var app = new Vue({
 	data: {
 		aAfishaItems: [
 			{
+				id: 0,
 				name: "Жёны артистов",
 				info: "\u0417\u0430\u0449\u0438\u0442\u0430 \u0437\u0432\u0430\u043d\u0438\u044f ''\u041d\u0430\u0440\u043e\u0434\u043d\u043e\u0433\u043e \u0442\u0435\u0430\u0442\u0440\u0430''||\u0412\u043e\u0437\u0440\u0430\u0441\u0442\u043d\u043e\u0435 \u043e\u0433\u0440\u0430\u043d\u0438\u0447\u0435\u043d\u0438\u0435 14+||\u041f\u0440\u0438\u0433\u043b\u0430\u0448\u0430\u0435\u043c \u0432\u0441\u0435\u0445!",
 				autor: "Антон Чехов",
@@ -198,14 +572,15 @@ var app = new Vue({
 				img: "imgs\/plays\/ArtistWifes.jpg",
 				day_date: "7 апреля",
 				day_name: "суббота",
-				time: "18:00",
+				time: "16:00",
 				place: "малый зал дк яуза",
 				age_limit: "14+",
 				dt: "2019-04-07",
 				tm: "16:00:00"
 			},
 			{
-				name: "Жёны артистов",
+				id: 1,
+				name: "Принцесса",
 				info: "Инфрмация",
 				autor: "Антон Чехов",
 				style: "Пьеса",
@@ -217,9 +592,10 @@ var app = new Vue({
 				place: "малый зал дк яуза",
 				age_limit: "14+",
 				dt: "2019-04-17",
-				tm: "16:00:00"
+				tm: "18:00:00"
 			},
 			{
+				id: 2,
 				name: "Жёны артистов",
 				info: "Инфрмация",
 				autor: "Антон Чехов",
@@ -228,13 +604,26 @@ var app = new Vue({
 				img: "img",
 				day_date: "7 мая",
 				day_name: "суббота",
-				time: "18:00",
+				time: "16:00",
 				place: "малый зал дк яуза",
 				age_limit: "14+",
 				dt: "2019-05-07",
 				tm: "16:00:00"
 			},
 		],
+		
+		editor: {
+			data: {
+				id: "99",
+				name: "99",
+				info: "99",
+				place: "99",
+				age_limit: "99",
+				dt: "2019-05-07",
+				tm: "16:00:00"
+			},
+			mode: "add"
+		},
 		aPlays: [
 			{
 				id: 1,
@@ -257,8 +646,7 @@ var app = new Vue({
 			{
 				name: "Историко-художественный музей",
 				url: ""
-			}
-			
+			}			
 		],
 		aAges: [
 			{
@@ -282,7 +670,9 @@ var app = new Vue({
 				"10": "Отктябрь",
 				"11": "Ноябрь",
 				"12": "Декабрь"
-			}
+		},
+		
+		bModalWinShow: false
 	},
 
 	computed: {
@@ -334,6 +724,48 @@ var app = new Vue({
 		
 	},
 	methods: {
+		showEditor: function(){
+			this.$refs.af_editor.init();
+			this.bModalWinShow = true;
+		},
+		closeModWin: function(){
+			this.bModalWinShow = false;
+		},
+		_clearEditor: function() {
+			for (let key in this.editor.data) {
+				this.editor.data[key] = "";
+			}
+		},
 		
+		editItem: function(nItemId){
+			this._clearEditor();
+			var oItem = this.aAfishaItems.find(el => el.id==nItemId);
+			if(oItem) {
+				for( let key in this.editor.data) {
+					this.editor.data[key] = String(oItem[key]);
+				}
+				this.editor.mode="edit";
+				this.showEditor();
+			}
+		},
+		
+		_sendData: function(sMode, oData) {
+			var aParams = [];
+			for(let key in oData) {
+				aParams.push(key +"="+ encodeURIComponent(oData[key]))
+			}
+			var sRequest = aParams.join("&");
+		},
+		
+		saveEdited: function(oData) {
+			this._sendData("save", oData);
+			this.closeModWin();
+		},
+		
+		addPlay: function() {
+			this._clearEditor();
+			this.editor.node="add";
+			this.showEditor();
+		}
 	}
 });
